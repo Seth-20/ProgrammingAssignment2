822b3be79fcfb23afaf096647a7b03b9730d4225
Seth_Silver <64823570+Seth-20@users.noreply.github.com>
2024-01-11 15:19:31 +0300

    Update cachematrix.R By Andruga Seth Silver
    Completed the MakeCacheMatrix and CacheSolve functions for programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
+#--------------------------------
+#The makeCacheMatrix creates a special “matrix” object that can store its 
+#inverse in a hidden environment.
+
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinverse <- function() m <<- inv(x)
+  getinverse<-function() m
 
 }
 
 
 ## Write a short comment describing this function
 
+#CacheSolve function computes the inverse of the matrix if it is not cached, 
+#or retrieves it from the cache if it is.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m<-x$getinverse
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get
+  m <- inv(data, ...)
+  x$setinverse(m)
+  m
 }
